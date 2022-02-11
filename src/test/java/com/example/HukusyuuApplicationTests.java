a28cea6543cbb9383d342d02be04dbfd2e46a650
seiyatadaka <seiya.tadaka@rakus-partners.co.jp>
2022-02-11 20:25:25 +0900

    復習開始

---
+++ src/test/java/com/example/HukusyuuApplicationTests.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HukusyuuApplicationTests {
+
+	@BeforeAll
+	static void beforeALL() {
+		System.out.println("beforeALL");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("afterAll");
+	}
+@BeforeEach
+	static void beforEach() {
+		System.out.println("beforEach");
+	}
+@AfterEach
+static void afterEach() {
+	System.out.println("afterEach");
+}
+
+	@Test
+	void test() {
+		System.out.println("test1");
+	}
+
+	@Test
+	void test2() {
+		System.out.println("test2");
+	}
+
+	
+
+	@Test
+	void contextLoads() {
+	}
+
+}
