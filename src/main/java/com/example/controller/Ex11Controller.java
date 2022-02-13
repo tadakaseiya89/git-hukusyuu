2bf6a578d418eebf5b15151204024f4b3355c760
seiyatadaka <seiya.tadaka@rakus-partners.co.jp>
2022-02-13 15:50:03 +0900

    演習10まで完了

---
+++ src/main/java/com/example/controller/Ex11Controller.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+
+@Controller
+@RequestMapping("/ex11")
+public class Ex11Controller {
+
+	@Autowired
+	private ServletContext application;
+
+	@RequestMapping("")
+	public String index() {
+		return "ex-11-input";
+	}
+
+	@RequestMapping("/input1")
+	public String input(User user) {
+		
+		application.setAttribute("user", user);
+		return "ex-11-output";
+	}
+
+	@RequestMapping("/output1")
+	public String output() {
+		return "ex-11-output";
+	}
+
+	@RequestMapping("/output3")
+	public String output2() {
+		return "ex-11-output2";
+	}
+
+}
